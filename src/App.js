8717e35e0bf8284e0706c4bb7fb14e0c9c15a765
JamieStelling <jamie14.stelling@mail.com>
2023-11-24 22:52:20 +0000

    App code

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import Form from "./components/Form"
+import Task from "./components/Task";
+import {useEffect, useState} from "react";
+
+function App() {
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks || []);
+  }, []);
+
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, {name:name,done:false}];
+    });
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks(prev => {
+      return prev.filter((taskObject,index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  function getMessage() {;
+    return 'Add a task';
+  }
+
+  function renameTask(index,newName) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    })
+  }
+
+  return (
+    <main>
+      <h1>To-Do App</h1>
+      <h2>{getMessage()}</h2>
+      <Form onAdd={addTask} />
+      {tasks.map((task,index) => (
+        <Task {...task}
+              onRename={newName => renameTask(index,newName)}
+              onTrash={() => removeTask(index)}
+              onToggle={done => updateTaskDone(index, done)} />
+      ))}
+    </main>
+  );
+}
+
+export default App;
